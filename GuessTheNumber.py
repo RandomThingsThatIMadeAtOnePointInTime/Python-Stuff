a5988207e28b8862b2244079c430a9ae7d59ef0b
Scarsz <me@scarsz.tech>
2016-09-29 13:16:19 -0700

    Initial commit

---
+++ GuessTheNumber.py
@@ -0,0 +1,35 @@
+import random
+
+maxNumber = 100
+correctNumber = random.randint(1, maxNumber)
+hasGuessedCorrectly = False
+guesses = 0
+maxGuesses = int(maxNumber / 15)
+
+def isnumber(x):
+    try:
+        int(x)
+        return True
+    except ValueError:
+        return False
+
+while not hasGuessedCorrectly and guesses < maxGuesses:
+    guess = input("What's the number? ")
+
+    if not isnumber(guess):
+        print("That's not a number, nerd")
+        continue
+
+    if int(guess) < correctNumber:
+        print("Too low")
+    if int(guess) > correctNumber:
+        print("Too high")
+    if int(guess) is correctNumber:
+        hasGuessedCorrectly = True
+    else:
+        guesses += 1
+
+if guesses is not maxGuesses:
+    print("Correct! The number was " + str(correctNumber))
+else:
+    print("You're not very smart. The number was " + str(correctNumber) + ". Better luck next time, looser.")
